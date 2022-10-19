9f1ea153139e2d6f31168c168658ce23f72502c2
Matheus Prado <peu_prado01@hotmail.com>
2022-10-19 20:08:35 -0300

    4. Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o component <NotFound.js >', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/1234/');
+    });
+
+    const notFound = screen.getByRole('heading', { level: 2, name: /Page requested not found/ });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/1234/');
+    });
+
+    const notFoundImg = screen.getByRole('img', { name: /Pikachu crying because the page requested was not found/ });
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
