8bc36f07eb0476c7ea02caa6c34f31d8f476a1d0
Matheus Prado <peu_prado01@hotmail.com>
2022-10-19 18:56:17 -0300

    2. Teste o componente <About.js />.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,46 @@
-test(' ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+    const infoPokedex = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+    expect(infoPokedex).toBeInTheDocument();
+
+    const infoPokedex2 = screen.getByText('One can filter Pokémons by type, and see more details for each one of them');
+    expect(infoPokedex2).toBeInTheDocument();
+
+    const aboutPokedex = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+
+    const aboutPokedex = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+
+    const aboutImage = screen.getByRole('img', { name: 'Pokédex' });
+    expect(aboutImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(aboutImage).toBeInTheDocument();
+  });
+});
